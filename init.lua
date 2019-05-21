4d95ca968b737c29658c392cda7d15ed3f8b6d50
Niwla23 <alwin.l@gmx.de>
2019-05-21 18:07:25 +0200

    update

--- init.lua
+++ init.lua
@@ -2,5 +2,5 @@
 
 local path = minetest.get_modpath("chisel")
 dofile(path .. "/nodes.lua")
-dofile(path .. "/chisel.lua")
-dofile(path .. "/api.lua")
+--dofile(path .. "/chisel.lua")
+--dofile(path .. "/api.lua")
---
+++ nodes.lua
@@ -0,0 +1,25 @@
+local S = minetest.get_translator(minetest.get_current_modname())
+
+minetest.register_node("chisel:dirt_bricks", {
+	description = S("Dirt Bricks"),
+	tiles = {"chisel_dirt_bricks.png"},
+	groups = {cracky=3, stone=1}
+})
+
+minetest.register_node("chisel:dirt_wih_dirt_bricks", {
+	description = S("Dirt with Dirt Bricks"),
+	tiles = {"chisel_dirt_bricks_top.png", "chisel_dirt_dirt_with_bricks.png"},
+	groups = {cracky=3, stone=1}
+})
+
+minetest.register_node("chisel:dirt_chunky", {
+	description = S("Chunky Dirt"),
+	tiles = {"chisel_dirt_chunky.png"},
+	groups = {cracky=3, stone=1}
+})
+
+minetest.register_node("chisel:dirt_cobble", {
+	description = S("Chunky Dirt"),
+	tiles = {"chisel_dirt_chunky.png"},
+	groups = {cracky=3, stone=1}
+})
