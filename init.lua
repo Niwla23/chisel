ca15e92da3ad8f0cdc84ddaba557b6e271429523
Niwla23 <alwin.l@gmx.de>
2019-05-22 15:45:18 +0200

    reorganizing structure

--- init.lua
+++ init.lua
@@ -2,4 +2,4 @@ chisel = {}
 
 dofile(minetest.get_modpath('chisel')..'/chisel.lua')
 dofile(minetest.get_modpath('chisel')..'/api.lua')
-dofile(minetest.get_modpath('chisel')..'/nodes.lua')
+dofile(minetest.get_modpath('chisel')..'/nodes/nodes.lua')
